b5d01576aaaeb901a9bfc729f3af173f3cccb5a5
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2022-02-12 21:18:12 +0100

    Création du controleur ApartmentController + création de la méthode list() + travail sur la vue Apartment/list.html.twig en cours

---
+++ src/Controller/ApartmentController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ApartmentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class ApartmentController extends AbstractController
+{
+    /**
+     * @Route("/apartment/list", name="apartment_list")
+     */
+    public function list(ApartmentRepository $apartmentRepository, UrlGeneratorInterface $urlGenerator): Response
+    {
+        $apartments = $apartmentRepository->findOneBy([], ['owner' => 'ASC'], null);
+
+        return $this->render('apartment/list.html.twig', [
+            'controller_name' => 'ApartmentController',
+            'apartments' => $apartments,
+            'urlGenerator' => $urlGenerator
+        ]);
+    }
+
+    /**
+     * @Route("/apartment", name="apartment_create")
+     */
+    public function create(): Response
+    {
+        return $this->render('apartment/index.html.twig', [
+            'controller_name' => 'ApartmentController',
+        ]);
+    }
+}
