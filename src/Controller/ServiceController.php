567eb8f14197d53e59712d31591e42261710777c
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2022-02-14 19:28:37 +0100

    Mise en place des Fixtures pour la classe Service + Création du controller ServiceController + vue Twig service/list.html.twig + ajout logo clé dans la Navbar à coté de Loki

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ServiceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class ServiceController extends AbstractController
+{
+    /**
+     * @Route("/service/list", name="service_list")
+     */
+    public function list(ServiceRepository $serviceRepository, UrlGeneratorInterface $urlGenerator): Response
+    {
+        $services = $serviceRepository->findBy([], ['label' => 'ASC']);
+
+        return $this->render('service/list.html.twig', [
+            'urlGenerator' => $urlGenerator,
+            'services' => $services
+        ]);
+    }
+
+    /**
+     * @Route("/service/{id}/edit", name="service_edit")
+     */
+    public function edit() {
+
+    }
+
+    /**
+     * @Route("/service/create", name="service_create")
+     */
+    public function create() {
+
+    }
+}
