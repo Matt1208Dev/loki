9551ba19b71e9b31b0140d6478b0055e90c85835
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2022-03-16 21:04:04 +0100

    Fix: modification migration concernnt la nullabilité du champ street

--- migrations/Version20220131231207.php
+++ migrations/Version20220131231207.php
@@ -20,12 +20,12 @@ final class Version20220131231207 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE owner ADD created_at DATETIME DEFAULT NULL, CHANGE street street VARCHAR(150) NOT NULL');
+        $this->addSql('ALTER TABLE owner ADD created_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE owner DROP created_at, CHANGE street street VARCHAR(150) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE owner DROP created_at CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
